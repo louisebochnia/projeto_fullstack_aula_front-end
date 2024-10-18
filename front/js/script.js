cd614e040d04c6c74dafb5422086021e797c6901
louisebochnia <louise.bochnia@gmail.com>
2024-10-18 09:13:19 -0300

    até estrutura novo filme

--- front/js/script.js
+++ front/js/script.js
@@ -5,6 +5,20 @@ const filmesEndpoint = '/filmes'
 async function obterFilmes() {
     const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`
     const filmes = (await axios.get(URLcompleta)).data
+    let tabela = document.querySelector('.filmes')
+    let corpoTabela = tabela.getElementsByTagName('tbody')[0]
+
+    for (let filme of filmes){
+        let linha = corpoTabela.insertRow(0)
+        let celulaTitulo = linha.insertCell(0)
+        let celulaSinopse = linha.insertCell(1)
+        celulaTitulo.innerHTML = filme.titulo
+        celulaSinopse.innerHTML = filme.sinopse
+    }
 
     console.log(filmes)
+}
+
+function cadastrarFilme() {
+    console.log("Vamos cadastrar")
 }
